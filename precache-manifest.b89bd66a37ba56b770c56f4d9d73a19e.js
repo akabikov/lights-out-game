166cbefed673c538742b5b68d795b58b533c9e16
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-05-02 16:57:02 +0300

    Updates

---
+++ precache-manifest.b89bd66a37ba56b770c56f4d9d73a19e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "48096a92c7e4270799a8",
+    "url": "/lights-out-game/static/css/main.0e55f600.chunk.css"
+  },
+  {
+    "revision": "48096a92c7e4270799a8",
+    "url": "/lights-out-game/static/js/main.48096a92.chunk.js"
+  },
+  {
+    "revision": "1288db43574d305fdd64",
+    "url": "/lights-out-game/static/js/1.1288db43.chunk.js"
+  },
+  {
+    "revision": "c21eb99d7eac6d163be2",
+    "url": "/lights-out-game/static/js/runtime~main.c21eb99d.js"
+  },
+  {
+    "revision": "0b34aa23a2412d8c63570fd775969b19",
+    "url": "/lights-out-game/index.html"
+  }
+];
